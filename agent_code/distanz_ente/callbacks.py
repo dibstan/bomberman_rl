54274af95639367a82fb2219796c1209496a56fa
hummerichsander <64867257+hummerichsander@users.noreply.github.com>
2021-03-12 17:56:02 +0100

    distanz features

---
+++ agent_code/distanz_ente/callbacks.py
@@ -0,0 +1,131 @@
+import os
+import pickle
+import random
+import sklearn as sk
+from sklearn.feature_extraction import DictVectorizer
+import numpy as np
+from itertools import product
+
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+
+def setup(self):
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    In this example, our model is a set of probabilities over actions
+    that are is independent of the game state.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    if self.train and not os.path.isfile("my-saved-model.pt"):
+        self.logger.info("Setting up model from scratch.")
+        ##weights = np.random.rand(len(ACTIONS))
+        self.model = None ## weights / weights.sum()
+    else:
+        self.logger.info("Loading model from saved state.")
+        with open("my-saved-model.pt", "rb") as file:
+            self.model = pickle.load(file)
+    print(self.model)
+
+    
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+    # todo Exploration vs exploitation
+    self.logger.info(state_to_features(game_state))
+    #self.logger.info(game_state['bombs'])
+    random_prob = 0.0
+
+    if self.train and random.random() < random_prob:
+        self.logger.debug("Choosing action according to the epsilon greedy policy.")
+        betas = list(self.model.values())
+        feature_vector = state_to_features(game_state)
+        
+        move = list(self.model.keys())[np.argmax(np.dot(betas, feature_vector))]
+        print(move)
+        return move
+
+    self.logger.debug("Querying model for action.")
+    return np.random.choice(ACTIONS, p=[.2,.2,.2,.2,.2,.0])
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.print(new_state_vector[0])
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+    if game_state is None:
+        return None
+    
+    channels = [[0,0] for i in range(2*17*17)]
+    
+    coordinates = np.array(list(product(np.arange(0,17),np.arange(0,17))))  # generating a list holding all possible coordinates of the field
+
+    position_self = np.array(game_state['self'][3])     # position of player self
+
+
+    # COINS
+    position_coins = np.array(game_state['coins'])      # position of the coins
+        
+    d_coins = position_coins - position_self   # distance from coins to player
+
+    for i in range(np.shape(position_coins)[0]):
+        channels[np.where((coordinates == position_coins[i]).all(axis=1))[0][0]] = d_coins[i]
+
+
+    # TILES
+    field=np.array([[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1,  0, -1],
+                    [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+                    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]])
+
+    rows, cols = np.where(field == -1)
+
+    position_tiles = np.array([rows, cols]).T
+    
+    d_tiles = position_tiles - position_self
+    
+    for i in range(np.shape(position_tiles)[0]):
+        channels[17*17+np.where((coordinates == position_tiles[i]).all(axis=1))[0][0]] = d_tiles[i]
+
+    # concatenate them as a feature tensor (they must have the same shape), ...
+    stacked_channels = np.stack(channels).reshape(-1)
+    # and return them as a vector
+    
+    return stacked_channels.reshape(-1)
---
+++ agent_code/distanz_ente/train.py
@@ -0,0 +1,224 @@
+import pickle
+import random
+from collections import namedtuple, deque
+from typing import List
+import numpy as np
+
+import events as e
+from .callbacks import state_to_features
+
+# nemad tuple for History
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+# Hyper parameters 
+TRANSITION_HISTORY_SIZE = 100  # keep only ... last transitions
+RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
+GAMMA = 0.3 # discount rate
+ALPHA = 0.5 # learning rate
+
+# Events
+WAITING_EVENT = "WAIT"
+COIN_CHASER = "CLOSER_TO_COIN"
+MOVED_AWAY_FROM_BOMB = "MOVED_AWAY_FROM_BOMB"
+WAITED_IN_EXPLOSION_RANGE = "WAITED_IN_EXPLOSION_RANGE"
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # Example: Setup an array that will note transition tuples
+    # (s, a, r, s')
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+
+    try:    # try loading existing model
+        with open("my-saved-model.pt", "rb") as file:
+            self.model = pickle.load(file)
+    except:     
+        self.model = None
+    
+    with open('explosion_map.pt', 'rb') as file:
+        self.exploding_tiles_map = pickle.load(file)
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+    self.logger.info(self_action)
+
+
+    # Auxillary events
+    if self_action == "WAIT":
+        events.append(WAITING_EVENT)
+
+    
+    #define auxillary events depending on the transition
+    if old_game_state is not None:
+
+        #get positions of the player
+        old_player_coor = old_game_state['self'][3]     
+        new_player_coor = new_game_state['self'][3]
+        
+        #define event coin_chaser
+        coin_coordinates = old_game_state['coins']
+        old_coin_distances = np.linalg.norm(np.subtract(coin_coordinates,old_player_coor),axis=0)
+        new_coin_distances = np.linalg.norm(np.subtract(coin_coordinates,new_player_coor),axis=0)
+
+        if max(new_coin_distances) > max(old_coin_distances):   #if the distance to closest coin got smaller
+            events.append(COIN_CHASER)
+
+        #define events with bombs
+        old_bomb_coors = old_game_state['bombs']
+
+        dangerous_tiles = []
+        for bomb in old_bomb_coors:
+            dangerous_tiles.append(self.exploding_tiles_map[bomb[0]])
+        
+        if dangerous_tiles != []:
+            if old_player_coor in dangerous_tiles and new_player_coor not in dangerous_tiles:
+                events.append(MOVED_AWAY_FROM_BOMB)
+            if old_player_coor in dangerous_tiles and self_action == "WAIT":
+                events.append(WAITED_IN_EXPLOSION_RANGE)
+        
+
+    # appending gamestate to history
+    self.transitions.append(Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward_from_events(self, events)))
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+
+    This is similar to reward_update. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+    self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
+    self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
+
+
+    # Updating the model
+    experience_replay(self)
+    
+
+    # Store the model
+    with open("my-saved-model.pt", "wb") as file:
+        pickle.dump(self.model, file)
+
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+   Auxillary rewards
+    """
+    game_rewards = {
+        e.COIN_COLLECTED: 500,
+        e.KILLED_OPPONENT: 5,
+        e.KILLED_SELF: -300,
+        WAITING_EVENT: -100,
+        e.INVALID_ACTION: -100,
+        #e.MOVED_DOWN: -40,
+        #e.MOVED_LEFT: -40,
+        #e.MOVED_RIGHT: -40,
+        #e.MOVED_UP: -40,
+        COIN_CHASER: 50,
+        MOVED_AWAY_FROM_BOMB: 50,
+        WAITED_IN_EXPLOSION_RANGE: -100  
+    
+    }
+    reward_sum = 0
+    
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
+
+def experience_replay(self):
+    self.logger.debug('Doing experience replay with the collected data.')
+
+    # creating the training batches from the transition history
+    B = {'UP':{'states':[], 'rewards':[], 'next_states':[]}, 'RIGHT':{'states':[], 'rewards':[], 'next_states':[]}, 'DOWN':{'states':[], 'rewards':[], 'next_states':[]}, 
+    'LEFT':{'states':[], 'rewards':[], 'next_states':[]}, 'WAIT':{'states':[], 'rewards':[], 'next_states':[]}, 'BOMB':{'states':[], 'rewards':[], 'next_states':[]}}
+
+    D = 0   # feature dimension
+
+    for transition in self.transitions:
+        if transition.action is not None:
+            B[transition.action]['states'].append(transition.state)
+            B[transition.action]['rewards'].append(transition.reward)
+            B[transition.action]['next_states'].append(transition.next_state)
+            D = len(transition.state)
+
+    
+    #initializing the model
+    if self.model is None:   
+        init_beta = np.zeros(D)
+        self.model = {'UP': init_beta, 
+        'RIGHT': init_beta, 'DOWN': init_beta,
+        'LEFT': init_beta, 'WAIT': init_beta, 'BOMB': init_beta}
+
+
+    # updating the model
+    for action in B:
+        X = B[action]['states']
+        Y_TD = []
+        Y = []
+
+        N = len(X)
+
+        if self.model is not None:
+            for i in range(N):
+
+                if B[action]['next_states'][i] is not None:     # not terminal state
+                    
+                    # computing the reward for the state according to temporal difference
+                    q_value_future = []
+
+                    for move in self.model:
+                        q_value_future.append(np.dot(self.model[move],B[action]['next_states'][i]))
+
+                    future_reward = np.max(q_value_future)
+
+                    
+                    Y_TD.append(B[action]['rewards'][i] + GAMMA * future_reward)
+                    
+                    # computing the predicted reward
+                    Y.append(np.dot(self.model[move], X[i]))
+
+                else:   # terminal state
+                    
+                    Y_TD.append(B[action]['rewards'][i])
+
+                    Y.append(np.dot(self.model[action], X[i]))
+
+            if X != []:
+                
+                DESC  = np.dot(np.transpose(X), np.array(Y_TD)-np.array(Y))    # gradient descent
+                
+                self.model[action] = self.model[action] + ALPHA * DESC
+                
+
+    print(self.model)
+
